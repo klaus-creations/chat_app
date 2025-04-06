0a5bc1f037486039a84eff0bada9feb6ab057de7
Abush Natanem <abushnatanem@gmail.com>
2025-04-06 19:44:54 +0300

    some change

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,11 @@
+import ChatWrapper from "@/components/chat/ChatWrapper";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <h1>First Next Setup</h1>
-      <Button>Click Here</Button>
+    <div className="size-full flex items-center justify-center">
+      <ChatWrapper />
     </div>
   );
 }
