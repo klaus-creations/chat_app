4bbe72c301a8fef1a0d9683cb9651367bf706217
Abush Natanem <abushnatanem@gmail.com>
2025-04-06 19:46:38 +0300

    side bar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SidebarHeader from "./SidebarHeader";
+import SidebarPeoplwNav from "./SidebarPeoplwNav";
 
 export default function Sidebar() {
   return (
@@ -8,8 +9,17 @@ export default function Sidebar() {
         <SidebarHeader />
       </header>
 
-      <section className="w-full h-[80%] py-1 overflow-x-hidden overflow-y-auto ">
-        Sidebar Main
+      <section className="w-full h-[80%] py-1 flex flex-col gap-3 overflow-x-hidden overflow-y-auto ">
+        {Array.from({ length: 20 }, (_, i) => {
+          return (
+            <SidebarPeoplwNav
+              img="github.com"
+              lastText={"hello world"}
+              name="Hello"
+              status={true}
+            />
+          );
+        })}
       </section>
     </aside>
   );
