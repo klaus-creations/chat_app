fd2d6bcfc85f3458efd72eb8d65cc37a2b0bf18e
Abush Natanem <abushnatanem@gmail.com>
2025-04-06 19:47:16 +0300

    update store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeSlice from "./features/slices/theme";
+import chatSlice from "./features/slices/chat"
 import { apiSlice } from "@/lib/features/api";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       theme: themeSlice,
+      chat: chatSlice,
       [apiSlice.reducerPath]: apiSlice.reducer,
+
     },
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(apiSlice.middleware), // Add the middleware for RTK Query
